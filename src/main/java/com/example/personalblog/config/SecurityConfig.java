64581988b09331e41fbbe9e9dc47861248e39e26
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-09 14:06:19 -0600

    Created ArticleService

--- src/main/java/com/example/personalblog/config/SecurityConfig.java
+++ src/main/java/com/example/personalblog/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.example.personalblog.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
-@Component
+@Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
--- src/main/java/com/example/personalblog/service/ArticleService.java
+++ src/main/java/com/example/personalblog/service/ArticleService.java
@@ -1,4 +1,12 @@
 package com.example.personalblog.service;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ArticleService {
+    private Path fileDirectory;
+
+    public ArticleService () {
+        this.fileDirectory = Paths.get ("articles");
+    }
 }
