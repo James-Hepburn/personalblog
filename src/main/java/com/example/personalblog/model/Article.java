64b6ee12b93319a7549f6687b19b7b6827caebd5
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-08 19:43:50 -0600

    Article class

--- src/main/java/com/example/personalblog/model/Article.java
+++ src/main/java/com/example/personalblog/model/Article.java
@@ -1,4 +1,37 @@
 package com.example.personalblog.model;
 
+import jakarta.persistence.Entity;
+import java.time.LocalDate;
+
+@Entity
 public class Article {
+    private String title;
+    private LocalDate date;
+    private String content;
+
+    public Article (String title, LocalDate date, String content) {
+        this.title = title;
+        this.date = date;
+        this.content = content;
+    }
+
+    public void setTitle (String title) {
+        this.title = title;
+    }
+    public void setDate (LocalDate date) {
+        this.date = date;
+    }
+    public void setContent (String content) {
+        this.content = content;
+    }
+
+    public String getTitle () {
+        return this.title;
+    }
+    public LocalDate getDate () {
+        return this.date;
+    }
+    public String getContent () {
+        return this.content;
+    }
 }
