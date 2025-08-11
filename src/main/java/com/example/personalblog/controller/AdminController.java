d3b174b5bba40167137807c1677047e1811a9dd6
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-10 21:10:49 -0600

    Admin edit and delete buttons

--- src/main/java/com/example/personalblog/controller/AdminController.java
+++ src/main/java/com/example/personalblog/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.example.personalblog.controller;
 
+import com.example.personalblog.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AdminController {
+    private ArticleService articleService;
+
+    public AdminController (ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
     @GetMapping("/admin")
     public String admin () {
         return "admin";
@@ -24,7 +31,19 @@ public class AdminController {
     }
 
     @GetMapping("/admin/dashboard")
-    public String dashboard () {
+    public String dashboard (Model model) {
+        model.addAttribute ("articles", articleService.getAllArticles ());
+
+        return "dashboard";
+    }
+
+    @GetMapping("/admin/edit")
+    public String edit () {
+        return "dashboard";
+    }
+
+    @GetMapping("/admin/delete")
+    public String delete () {
         return "dashboard";
     }
 }
