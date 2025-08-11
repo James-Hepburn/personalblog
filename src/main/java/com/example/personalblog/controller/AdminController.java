74556c2c57f9601274f171a3c5a991eed605dc34
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-10 20:50:14 -0600

    Admin login page

--- src/main/java/com/example/personalblog/controller/AdminController.java
+++ src/main/java/com/example/personalblog/controller/AdminController.java
@@ -1,4 +1,29 @@
 package com.example.personalblog.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class AdminController {
+    @GetMapping("/admin")
+    public String admin () {
+        return "admin";
+    }
+
+    @GetMapping("/admin/login")
+    public String login (@RequestParam String username, @RequestParam String password, Model model) {
+        if (username.equals ("admin") && password.equals ("admin123")) {
+            return "redirect:/admin/dashboard";
+        } else {
+            model.addAttribute ("error", "Incorrect username or password");
+            return "admin";
+        }
+    }
+
+    @GetMapping("/admin/dashboard")
+    public String dashboard () {
+        return "dashboard";
+    }
 }
