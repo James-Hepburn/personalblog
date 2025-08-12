2804c4cedc295056bf0a80e82b8e01c1a21797e3
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-11 19:37:16 -0600

    Final version

--- src/main/java/com/example/personalblog/controller/GuestController.java
+++ src/main/java/com/example/personalblog/controller/GuestController.java
@@ -1,9 +1,11 @@
 package com.example.personalblog.controller;
 
+import com.example.personalblog.model.Article;
 import com.example.personalblog.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class GuestController {
@@ -20,8 +22,15 @@ public class GuestController {
         return "index";
     }
 
-    @GetMapping ("/article")
-    public String viewArticle (Model model) {
-        return "index";
+    @GetMapping("/article/{fileName}")
+    public String viewArticle (@PathVariable String fileName, Model model) {
+        Article article = articleService.getArticleByFileName (fileName);
+
+        model.addAttribute ("fileName", fileName);
+        model.addAttribute ("title", article.getTitle ());
+        model.addAttribute ("date", article.getDate ());
+        model.addAttribute ("content", article.getContent ());
+
+        return "viewArticle";
     }
 }
